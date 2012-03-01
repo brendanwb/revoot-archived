a67ed8e5878b20677c2947c99e466f8c18c9bb0d
brendanwb <brendan.buckingham.com>
2012-02-29 21:24:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevootApp::Application.config.secret_token = 'a92ce2480c2fda648d3eb744447ba29d9080dc503379d8c3c40822e0cd3cf662d8b3529218023d277da975f1d470aa2b683d9bd95884029cd034f25b13e4963c'
