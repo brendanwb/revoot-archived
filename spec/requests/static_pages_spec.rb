954e34114daebb3c2765b6fcf24628daed631bdb
brendanwb <brendan.buckingham.com>
2012-04-16 16:27:13 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,54 +1,57 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  # subject { page } allows us to eliminate the sources of 
+  # duplication of stating it "should have xxxx" and page.should have_xxxx
+  # by telling RSpec that page is the subject of all the tests
+  subject { page }
   
   describe "Home page" do
     
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    # example of old test to compare to updated tests below
+    # it "should have content 'Help'" do
+    #   visit help_path
+    #   page.should have_selector('h1', :text => 'Help')
+    # end
     
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', 
-                      :text => "Revoot")
-    end
+    # use before to eliminate the duplication of stating visit 
+    # root_path in each test
+    before { visit root_path }
     
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    # using a variant of the it method to collapse the code and 
+    # description into one line like below
+    it { should have_selector('h1', text: 'Revoot') }
+    # full_title is used just like in the helper for the actual view.  It is 
+    # stored in spec/support/utilities.rb and is available to all RSpec tests
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
   end
   
   describe "Help page" do
-    
-    it "should have content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', 
-                      :text => "Revoot | Help")
-    end
+
+    before { visit help_path }
+
+    # the line "should have content 'Help'" do was removed as a result of
+    # adding the subject { page } to the beginning of the page. brackets
+    # are used instead of a do end block
+    # all hashes have been changed from :text => 'Help' to new 1.9.3 simplified
+    # text: 'Help' style.
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
   
   describe "About page" do
     
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+    before { visit about_path }
     
-    it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', 
-                      :text => "Revoot | About Us")
-    end
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
   
-  
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
   
 end
