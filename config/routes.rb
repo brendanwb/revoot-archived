354efbe82e02ae0e603d336e62e920ea13de2edf
brendanwb <brendan.buckingham.com>
2012-05-14 22:57:24 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 RevootApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -8,6 +9,8 @@ RevootApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # See how all your routes lay out with "rake routes"
 
