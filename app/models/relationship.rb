a0b18afdebb896d9bca2e5b4a8cc72b5eafa843d
brendanwb <brendan.buckingham.com>
2012-10-25 22:22:24 -0500

    Relationships deleted

--- app/models/relationship.rb
+++
@@ -1,9 +0,0 @@
-class Relationship < ActiveRecord::Base
-  attr_accessible :tv_show_id
-
-  belongs_to :user
-  belongs_to :tv_show
-
-  validates :user_id, presence: true
-  validates :tv_show_id, presence: true
-end
