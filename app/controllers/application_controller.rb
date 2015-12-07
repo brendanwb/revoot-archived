86217c52fc40ccf91db8020a5b1007248e203a03
Brendan Buckingham <brendan.buckingham@gmail.com>
2015-12-07 14:55:50 -0600

    test hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  def hello_world
+    if this_is_a_really_really_really_really_really_really_really_really_really_long_variable
+      true
+    else
+      'false'
+    end
+  end
+
+  def this_is_a_really_really_really_really_really_really_really_really_really_long_variable
+    true
+  end
 end
